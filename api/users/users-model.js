662ecd4691b2faacc30366f16bbcca6f4de2ab52
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-10 07:10:14 -0500

    users get router is working. model has been updated.

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -3,21 +3,25 @@ const db = require('../../data/db-config')
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-  return db('users')
+  return db('users').select('user_id', 'username')
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-  return db('users').where(filter)
+  return db('users')
+  .select('user_id', 'username')
+  .where(filter)
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-  return db('users').where('user_id', user_id).first()
+  return db('users')
+  .select('user_id', 'username')
+  .where('user_id', user_id).first()
 }
 
 /**
